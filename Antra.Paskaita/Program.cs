311c2f0d3a63d3c9c702f82877449a79f47eabc9
Augustas Tupciauskas <augustast@icloud.com>
2025-10-25 16:57:59 +0300

    išanalizuota paskaitos tema

--- Antra.Paskaita/Program.cs
+++ Antra.Paskaita/Program.cs
@@ -23,13 +23,20 @@
             string emptyStringExample2 = string.Empty;
             string emptyStringExample3;        // tik tokiu atveju jis nėra tuščias, greičiau atlieka kaip 'null' reikšmę.
 
+            char[] lettersMasyvas = {'ž','o','d','i','s'};
+            string masyvas = new string(lettersMasyvas);
+            Console.WriteLine(lettersMasyvas);
+            Console.WriteLine(masyvas);        // nors atspausdina vienodai, bet veikia skirtingai 'masyvas' yra string, o 'lettersMasyvas' yra char[]
 
+            string word1 = "mama";
+            string word2 = word1.Replace('m', 'p');
+            Console.WriteLine(word1); // mama
+            Console.WriteLine(word2); // papa
 
+            Console.WriteLine(word1.Length);
 
-
-
-
-
+            string word3 = "1234567890"; 
+            Console.WriteLine(word3.Length); //word3 turi 10 charų (simboliu)
 
             Console.WriteLine();
             Console.WriteLine(@"Užduotis 01");
