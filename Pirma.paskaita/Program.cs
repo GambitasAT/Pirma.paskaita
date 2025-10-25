12790a7fbdc600311fa46bd93bdde48b264fa226
Augustas Tupciauskas <augustast@icloud.com>
2025-10-25 15:32:10 +0300

    3,4,5,6 uzduotys

--- Pirma.paskaita/Program.cs
+++ Pirma.paskaita/Program.cs
@@ -34,9 +34,70 @@ namespace Pirma.paskaita
             Console.WriteLine("užduotis 03");
             Console.WriteLine();
             Console.WriteLine(@"   /\   ");
+            Console.ForegroundColor = ConsoleColor.Green;
             Console.WriteLine(@"  /  \  ");
+            Console.ForegroundColor = ConsoleColor.Red;
             Console.WriteLine(@" /    \ ");
+            Console.ForegroundColor = ConsoleColor.Magenta;
             Console.WriteLine(@"/______\");
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine();
+            Console.WriteLine("užduotis 04");
+            Console.WriteLine();
+            Console.ForegroundColor = ConsoleColor.DarkBlue;
+            Console.WriteLine(@"  / \     / \  ");
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine(@" /   \   /   \ ");
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine(@" \    \ /    / ");
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine(@"  \         /  ");
+            Console.ForegroundColor = ConsoleColor.DarkYellow;
+            Console.WriteLine(@"   \       /   ");
+            Console.ForegroundColor = ConsoleColor.Magenta;
+            Console.WriteLine(@"    \     /    ");
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine(@"     \   /     ");
+            Console.ForegroundColor = ConsoleColor.DarkRed;
+            Console.WriteLine(@"      \ /      ");
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine(@"       V       ");
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine();
+            Console.WriteLine("užduotis 05");
+            Console.WriteLine();
+            string preke = "Obuolys";
+            int kiekis = 5;
+            int kaina = 2;
+            Console.WriteLine("Prekė : " + preke);
+            Console.WriteLine("Kiekis: " + kiekis);
+            Console.WriteLine("Kaine : " + kaina + " eur");
+            Console.WriteLine();
+            Console.WriteLine("užduotis 06");
+            Console.WriteLine();
+            string vardas = "Vardenis";
+            string pavarde = "Pavardenis";
+            int amzius = 28;
+            string pareigos = "Software Developer";
+            string eMail = "vardenis.pavardenis@example.com";
+            string tel = "+1 123-456-7890";
+            Console.WriteLine(@"============== Vizitinė ==============");
+            Console.WriteLine(@"======================================");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
         }
     }
 }
