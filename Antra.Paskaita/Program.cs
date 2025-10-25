b2cfb9ca2399cc1fa970e21434e913b23aca9b71
Augustas Tupciauskas <augustast@icloud.com>
2025-10-25 16:11:47 +0300

    added antra paskaita

---
+++ Antra.Paskaita/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Antra.Paskaita
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
