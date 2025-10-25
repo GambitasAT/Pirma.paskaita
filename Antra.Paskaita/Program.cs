a83f0c0cca54083ed08646974baa9e3eace12128
Augustas Tupciauskas <augustast@icloud.com>
2025-10-25 16:29:41 +0300

    string variations

--- Antra.Paskaita/Program.cs
+++ Antra.Paskaita/Program.cs
@@ -6,8 +6,35 @@
         {
             Console.WriteLine("Paskaita 02");
             Console.WriteLine();
+
+            string firstLine = "vienas";
+            string secondLine = "du";
+            firstLine = firstLine + " " + secondLine;
+            Console.WriteLine(firstLine);
+            secondLine += firstLine;
+            Console.WriteLine(secondLine);     //awoiding due string name conflicts
+            Console.WriteLine();
+            string city = "Vilnius";           // string priskyrimas
+            int wordLenght = "Vilnius".Length; // priskiriame tikrinti žodžio ilgį
+            int wordLenght2 = city.Length;     // priskiriame tikrinti 'string' žodžio ilgį
+                                               // pastaba prisiminti skaičiavimas vyksta nuo 0 o ne nuo 1
+
+            string emptyStringExample = "";
+            string emptyStringExample2 = string.Empty;
+            string emptyStringExample3;        // tik tokiu atveju jis nėra tuščias, greičiau atlieka kaip 'null' reikšmę.
+
+
+
+
+
+
+
+
+
+            Console.WriteLine();
             Console.WriteLine(@"Užduotis 01");
             Console.WriteLine();
+
 
         }
     }
