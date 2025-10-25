e3f6c8622b8b8db2d1706d09e05decb3d1afae9f
Augustas Tupciauskas <augustast@icloud.com>
2025-10-25 14:49:47 +0300

    Add project files.

---
+++ Pirma.paskaita/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Pirma.paskaita
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Labas, pasauli");
+            Console.WriteLine("kiek");
+            int Vienas = 1;
+            string Zodis = "zodis";
+
+            Console.WriteLine(Vienas + " " + Zodis);
+
+            Console.WriteLine(@"   /\   ");
+            Console.WriteLine(@"  /  \  ");
+            Console.WriteLine(@" /    \ ");
+            Console.WriteLine(@"/______\");
+
+
+        }
+    }
+}
