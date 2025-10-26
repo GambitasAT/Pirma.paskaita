acb4afc3dc246542f5d4736cb69916b85727ede0
Augustas Tupciauskas <augustast@icloud.com>
2025-10-26 11:33:06 +0200

    added trecia paskaita

---
+++ Trecia.Paskaita/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Trecia.Paskaita
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
